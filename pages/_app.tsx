013f5d890e01a71d28744a110fbca183c3c724c8
timwatts80 <tim_watts@ymail.com>
2023-07-27 15:24:35 -0600

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app'
 import Header from '../components/Header/Header'
 import ModernWorkplace from '../components/ModernWorkplace/ModernWorkplace'
 import ProductSectionSurfaceHubDevice from '../components/ProductSectionSurfaceHubDevice/ProductSectionSurfaceHubDevice'
+import CustomerCases from 'components/CustomerCases/CustomerCases'
 import Footer from '../components/Footer/Footer'
 import { ThemeProvider } from '@emotion/react'
 
@@ -13,6 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Header />
       <ModernWorkplace />
       <ProductSectionSurfaceHubDevice />
+      <CustomerCases />
       <Footer />
     </ThemeProvider>
   )
