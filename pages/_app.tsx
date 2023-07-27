b6dcddd8bf50af6d3bc3c4dea89d21a01a734214
timwatts80 <tim_watts@ymail.com>
2023-07-26 18:47:57 -0600

    added components to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
+import MowoDesignSystemDarkTheme from '../MowoDesignSystemDarkTheme'
+import MowoDesignSystemLightTheme from '../MowoDesignSystemLightTheme'
+
 import type { AppProps } from 'next/app'
+import Header from '../components/Header/Header'
+import ModernWorkplace from '../components/ModernWorkplace/ModernWorkplace'
+import ProductSectionSurfaceHubDevice from '../components/ProductSectionSurfaceHubDevice/ProductSectionSurfaceHubDevice'
+import Footer from '../components/Footer/Footer'
+import { ThemeProvider } from '@emotion/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={MowoDesignSystemLightTheme}>
+      <Header />
+      <ModernWorkplace />
+      <ProductSectionSurfaceHubDevice />
+      <Footer />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
