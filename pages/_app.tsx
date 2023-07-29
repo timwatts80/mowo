2a18d913eb8ebc199ac654baaf9d6e3f3636a31d
timwatts80 <tim_watts@ymail.com>
2023-07-29 08:37:07 -0600

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,17 @@ import type { AppProps } from 'next/app'
 import Header from '../components/Header/Header'
 import ModernWorkplace from '../components/ModernWorkplace/ModernWorkplace'
 import ProductSectionSurfaceHubDevice from '../components/ProductSectionSurfaceHubDevice/ProductSectionSurfaceHubDevice'
+import ReactSimplyCarouselExample from '../components/ReactSimplyCarouselExample/ReactSimplyCarouselExample'
 import CustomerCases from 'components/CustomerCases/CustomerCases'
 import Footer from '../components/Footer/Footer'
 import { ThemeProvider } from '@emotion/react'
+import React from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={MowoDesignSystemLightTheme}>
       <Header />
+      <ReactSimplyCarouselExample />
       <ModernWorkplace />
       <ProductSectionSurfaceHubDevice />
       <CustomerCases />
