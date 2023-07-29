39e6e4ed8a234eee9bb46947d5df35c27356c7b5
timwatts80 <tim_watts@ymail.com>
2023-07-29 09:06:28 -0600

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,11 +9,15 @@ import CustomerCases from 'components/CustomerCases/CustomerCases'
 import Footer from '../components/Footer/Footer'
 import { ThemeProvider } from '@emotion/react'
 import React from 'react'
+import ProductList from 'components/ProdustList/ProdustList'
+import ProductBox1 from 'components/ProductBox1/ProductBox1'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={MowoDesignSystemLightTheme}>
       <Header />
+      <ProductBox1 />
+      <ProductList />
       <ReactSimplyCarouselExample />
       <ModernWorkplace />
       <ProductSectionSurfaceHubDevice />
