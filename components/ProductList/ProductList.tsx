9e4422c83bfd303bcc9b72138f80d56a014183c3
timwatts80 <tim_watts@ymail.com>
2023-07-31 14:01:20 -0600

    update

--- components/ProductList/ProductList.tsx
+++ components/ProductList/ProductList.tsx
@@ -43,7 +43,8 @@ function ProductList(props: ProductListProps): JSX.Element {
     <ProductList1 className={props.className}>
       {data.products &&
         data.products.map((product: any, index: number) => {
-          return <ProductCard1 key={index} isProduct2={product.isProduct2}/>;
+          const isProductPropName = `isProduct${index + 1}`; // generate the prop name based on the current index
+          return <ProductCard1 key={index} {...{ [isProductPropName]: true }} />;
         })}
     </ProductList1>
   );
