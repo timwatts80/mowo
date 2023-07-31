42663d182b771a192fe51900b981da2b80585b41
timwatts80 <55103053+timwatts80@users.noreply.github.com>
2023-07-31 14:48:38 -0600

    Imported from Quest

--- types.ts
+++ types.ts
@@ -81,6 +81,7 @@
 
 
 
+
 export interface ProductCardProps {
   className?: any;
   isProduct?: boolean;
@@ -90,6 +91,7 @@ export interface ProductCardProps {
   isProduct5?: boolean;
   isProduct6?: boolean;
   };
+
 
 
 
@@ -212,6 +214,7 @@ export interface ProductListProps {
 
 
 
+
 export interface ProductBoxProps {
   className?: any;
   id?: number;
@@ -264,9 +267,11 @@ export interface ProductBoxProps {
 
 
 
+
 export interface ProductBox2Props {
   className?: any;
 };
+
 
 
 
@@ -397,6 +402,7 @@ export interface ProductSectionSurfaceHubDeviceProps {
 
 
 
+
 export interface ProdustListProps {
   className?: any;
 };
@@ -451,9 +457,11 @@ export interface ProdustListProps {
 
 
 
+
 export interface ProductBox1Props {
   className?: any;
 };
+
 
 
 
@@ -572,6 +580,7 @@ export interface Frame139Props {
 
 
 
+
 export interface ProductBox7Props {
   className?: any;
 };
@@ -630,9 +639,11 @@ export interface ProductBox7Props {
 
 
 
+
 export interface ProductBox6Props {
   className?: any;
 };
+
 
 
 
@@ -749,9 +760,11 @@ export interface ProductBox5Props {
 
 
 
+
 export interface ProductBox4Props {
   className?: any;
 };
+
 
 
 
@@ -872,9 +885,11 @@ export interface ProductBox3Props {
 
 
 
+
 export interface CustomerCardProps {
   className?: any;
 }
+
 
 
 
@@ -1012,9 +1027,11 @@ export interface ModernWorkplaceProps {
 
 
 
+
 export interface NavbarProps {
   className?: any;
 }
+
 
 
 
@@ -1152,9 +1169,11 @@ export interface HeaderProps {
 
 
 
+
 export interface SidebarProps {
   className?: any;
 }
+
 
 
 
@@ -1292,6 +1311,7 @@ export interface Link1Props {
 
 
 
+
 export interface CustomerCasesProps {
   className?: any;
 }
@@ -1362,9 +1382,11 @@ export interface CustomerCasesProps {
 
 
 
+
 export interface FooterProps {
   className?: any;
 }
+
 
 
 
