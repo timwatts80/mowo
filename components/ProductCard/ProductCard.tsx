05cdd59b1633ff88dc13da5b8ceaae81732553e0
timwatts80 <tim_watts@ymail.com>
2023-08-06 21:21:29 -0600

    update

--- components/ProductCard/ProductCard.tsx
+++ components/ProductCard/ProductCard.tsx
@@ -232,7 +232,7 @@ function ProductCard(props: ProductCardProps): JSX.Element {
           </Bottomaccent>
         </ProductCardTemp>
       </Productcardcontainer>
-      <Dialog open={data.isDialogOpen} onClose={fns.toggleDialog} maxWidth="false">
+      <Dialog open={data.isDialogOpen} onClose={fns.toggleDialog}>
         <Productdialogcontain>
           <ProductPageTest1
             open={data.isDialogOpen}
