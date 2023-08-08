67c19da88b65b4e924e8f0d3cfb0dbab05da265d
timwatts80 <tim_watts@ymail.com>
2023-08-08 07:58:15 -0600

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,7 +22,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={MowoDesignSystemLightTheme}>
       <Navbar />
-      <ProductCarouselTemp />
       <Header />
       <ModernWorkplace />
       <ProductSectionSurfaceHubDevice />
