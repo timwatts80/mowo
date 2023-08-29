1552774bc95eb3ad22971837852637c84253becc
timwatts80 <55103053+timwatts80@users.noreply.github.com>
2023-08-28 22:29:41 -0600

    Imported from Quest

--- types.ts
+++ types.ts
@@ -110,10 +110,12 @@
 
 
 
+
 export interface SidebarcontentProps {
   className?: any;
   onClose?: any;
 };
+
 
 
 
@@ -345,11 +347,13 @@ export interface SidebarProps {
 
 
 
+
 export interface NavbarProps {
   className?: any;
   open?: any;
   onClose?: any;
 }
+
 
 
 
@@ -705,6 +709,7 @@ export interface Link1Props {
 
 
 
+
 export interface ContentcontainerProps {
   className?: any;
   products?: any;
@@ -798,6 +803,7 @@ export interface ContentcontainerProps {
 
 
 
+
 export interface ProductCardProps {
   className?: any;
   products?: any;
@@ -812,6 +818,7 @@ export interface ProductCardProps {
     alt: string;
   };
 };
+
 
 
 
@@ -1172,6 +1179,7 @@ export interface ProductDialogProps {
 
 
 
+
 export interface ClientCaseStudyProps {
   className?: any;
   bgimage: {
@@ -1181,6 +1189,7 @@ export interface ClientCaseStudyProps {
   title: string;
   description: string;
 };
+
 
 
 
@@ -1474,6 +1483,7 @@ export interface ProductCardNewProps {
 
 
 
+
 export interface ClientCarouselProps {
   className?: any;
 };
@@ -1565,9 +1575,11 @@ export interface ClientCarouselProps {
 
 
 
+
 export interface CarouselContainerProps {
   className?: any;
 };
+
 
 
 
@@ -1924,9 +1936,11 @@ export interface FooterProps {
 
 
 
+
 export interface HeaderProps {
   className?: any;
 }
+
 
 
 
@@ -2223,6 +2237,7 @@ export interface ClientLogoCardProps {
 
 
 
+
 export interface ClientCaseCarouselProps {
   className?: any;
 
@@ -2342,9 +2357,11 @@ export interface ClientCaseCarouselProps {
 
 
 
+
 export interface CustomerCasesProps {
   className?: any;
 }
+
 
 
 
@@ -2693,6 +2710,7 @@ export interface TableProps {
 
 
 
+
 export interface ProductCardAnimProps {
   className?: any;
   product_id: number;
@@ -2703,6 +2721,7 @@ export interface ProductCardAnimProps {
     alt: string;
   };
 };
+
 
 
 
@@ -3039,11 +3058,13 @@ export interface ProductCard1Props {
 
 
 
+
 export interface ProductDialog1Props {
   className?: any;
   open: any;
   onClose: any;
 };
+
 
 
 
@@ -3396,12 +3417,14 @@ export interface ProductPageTestProps {
 
 
 
+
 export interface ProductPageProps {
   className?: any;
   open: any;
   onClose: any;
   products: any;
 };
+
 
 
 
@@ -3751,10 +3774,12 @@ export interface ProductCardListProps {
 
 
 
+
 export interface ProductCardTempProps {
   className?: any;
   products: any;
 };
+
 
 
 
@@ -4106,6 +4131,7 @@ export interface MenulinklistProps {
 
 
 
+
 export interface ContactUsProps {
   className?: any;
 };
@@ -4283,9 +4309,11 @@ export interface ContactUsProps {
 
 
 
+
 export interface ModernWorkplaceProps {
   className?: any;
 }
+
 
 
 
@@ -4641,11 +4669,13 @@ export interface Card27Props {
 
 
 
+
 export interface ProductModalProps {
   className?: any;
   open: any;
   onClose: any;
 };
+
 
 
 
@@ -4996,11 +5026,13 @@ export interface DialogSampleProps {
 
 
 
+
 export interface ContactUsSampleProps {
   className?: any;
   open: any;
   onClose: any;
 };
+
 
 
 
@@ -5351,9 +5383,11 @@ export interface ProductSectionSurfaceHubDeviceProps {
 
 
 
+
 export interface ProductCarouselTempProps {
   className?: any;
 };
+
 
 
 
@@ -5707,9 +5741,11 @@ export interface ProductBoxProps {
 
 
 
+
 export interface ProductBox2Props {
   className?: any;
 };
+
 
 
 
@@ -6057,9 +6093,11 @@ export interface ProductListProps {
 
 
 
+
 export interface ProductBox1Props {
   className?: any;
 };
+
 
 
 
@@ -6409,9 +6447,11 @@ export interface Frame139Props {
 
 
 
+
 export interface ProductBox7Props {
   className?: any;
 };
+
 
 
 
@@ -7125,9 +7165,11 @@ export interface ProductBox6Props {
 
 
 
+
 export interface ProductBox5Props {
   className?: any;
 };
+
 
 
 
@@ -7675,6 +7717,7 @@ export interface ProductBox4Props {
 
 
 
+
 export interface ProductBox3Props {
   className?: any;
 };
@@ -7866,9 +7909,11 @@ export interface ProductBox3Props {
 
 
 
+
 export interface CustomerCardProps {
   className?: any;
 }
+
 
 
 
