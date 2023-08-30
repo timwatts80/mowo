130153e948b8f5ec974c6df23462425f6727d5de
timwatts80 <tim_watts@ymail.com>
2023-08-29 18:16:09 -0600

    update

--- components/ClientCarousel/ClientCarousel.tsx
+++ components/ClientCarousel/ClientCarousel.tsx
@@ -10,22 +10,22 @@ import styled from '@emotion/styled';
 import { useMediaQuery } from '@mui/material';
 
 const CarouselContainer: any = styled('div')(({ data }: any) => {
-  const ScreenMobile = useMediaQuery('(max-width:497px)');
-  const ScreenTablet = useMediaQuery('(max-width:999px)');
-  const paddingValue = ScreenMobile ? `0px` : ScreenTablet ? `30px` : `100px`;
+    const ScreenMobile = useMediaQuery('(max-width:497px)');
+    const ScreenTablet = useMediaQuery('(max-width:999px)');
+    const paddingValue = ScreenMobile ? `0px` : ScreenTablet ? `30px` : `100px`;
 
-  return {
-    position: `relative`,
-    isolation: `isolate`,
-    flexDirection: `row`,
-    width: '100%',
-    justifyContent: `flex-start`,
-    alignItems: `center`,
-    padding: paddingValue,
-    margin: '50px auto 125px',
-    boxSizing: `border-box`,
-    height: 'auto',
-  };
+    return {
+        position: `relative`,
+        isolation: `isolate`,
+        flexDirection: `row`,
+        width: '100%',
+        justifyContent: `flex-start`,
+        alignItems: `center`,
+        padding: paddingValue,
+        margin: '50px auto 125px',
+        boxSizing: `border-box`,
+        height: 'auto',
+    };
 });
 
 const MainImage: any = styled('div')({
@@ -39,12 +39,19 @@ function CarouselTest(props: ClientCaseCarouselProps): JSX.Element {
 
     const settings = {
         dots: true,
-        slidesToShow: 1,
+        slidesToShow: 2,
         slidesToScroll: 1,
         infinite: true,
         arrows: true,
         responsive: [
             // Responsive settings
+            {
+                breakpoint: 572, // mobile breakpoint
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
         ],
     };
 
